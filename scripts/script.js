42803dbdd2da9ff80de09bcd7a230f419cfae6c9
Elza Yusufova <elza.iusufova@gmail.com>
2024-01-11 12:18:23 -0500

    merging to main

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// function doAnimation() {
+//   var targetElement = document.getElementById("target");
+//   targetElement.className = "animated";
+// }
+
+const btn = document.getElementById("target");
+
+function doAnimation() {
+  btn.innerHTML = "Clicked ME";
+  // btn.style = "animated";
+}
+
+// console.log(doAnimation());
+// // Add a click event handler (via an anonymous inline arrow function)
+// btn.addEventListener("click", (event) => {
+//   console.log(event);
+// });
+
+// console.log("hello");
