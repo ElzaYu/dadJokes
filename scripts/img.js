dc15ba88950d1032754f3357dabb70f2ace3a7c8
charuzain <charuabhizain@gmail.com>
2024-01-11 11:33:52 -0500

    Fetch Joke as an Image

--- scripts/img.js
+++ scripts/img.js
@@ -30,10 +30,14 @@ imgContainer.appendChild(img);
 const jokeBtn = document.querySelector('.btn');
 
 jokeBtn.addEventListener('click', () => {
+  const imageId = ['0DdFQZgyXnb', '0ga2wsPZgib', '2giGYLeiGe', '5TS7pbxcpb', '69xAsrHYDAd', '82wHlbaapzd', '8hFYojqz5h', '9hiGeNZ0Tnb', 'yP7MRucFQCd', 'a218pbMmOmb', 'FtHJmGJ61wc', 'aMmbaFYTKBd', 'caxscaMRnjb', 'ci311DtH6h', 'HQfNZDIRSnb', 'HeiqcaMRKBd', 'IRKJBQ7p4wc', 'exXSCtkOKe', 'JJ61L61Lusc', 'f211DdFBd']
+  const randomImgId = Math.floor(Math.random() * imageId.length);
 
-  console.log("l")
+  const randomJoke = imageId[randomImgId];
+  console.log(randomJoke)
+
   const jokeContainer = document.querySelector('.img-container')
   jokeContainer.innerHTML = ""
-fetchImgJokes("M7wPC5wPKBd")
+fetchImgJokes(randomJoke)
 })
 
