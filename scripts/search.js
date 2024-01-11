bea4fa0f6aae08231c5c7799e508098b398b4f56
charuzain <charuabhizain@gmail.com>
2024-01-10 22:23:08 -0500

    Display total number of jokes found with a term

--- scripts/search.js
+++ scripts/search.js
@@ -1,14 +1,30 @@
 
 const form = document.querySelector('form')
 
+const displaySeachedJokes = (searchedJokeList , searchTerm) => {
+  console.log(searchedJokeList)
+  const jokeListWrapper = document.querySelector('.joke-list-wrapper')
+  console.log(jokeListWrapper)
+  const totalJokes = searchedJokeList.length || "No"
+  
+  const h2 = document.createElement('h2')
+  h2.textContent = `${totalJokes} jokes found with term ${searchTerm}`
+  jokeListWrapper.appendChild(h2)
 
-const serachJokesByTerm = async(serachTerm) => {
+}
+
+
+const serachJokesByTerm = async(searchTerm) => {
   try {
-    const url = `https://icanhazdadjoke.com/search?term=${serachTerm}`
+    const url = `https://icanhazdadjoke.com/search?term=${searchTerm}`
     const response = await axios.get(url, { headers: { "Accept": "application/json" } })
     const searchedJokeList = response.data.results
     console.log(searchedJokeList)
-    
+
+    const jokesListWrapper = document.querySelector('.joke-list-wrapper')
+    console.log(jokesListWrapper)
+    jokesListWrapper.innerHTML =""
+    displaySeachedJokes(searchedJokeList , searchTerm)  
   } catch (error) {
     console.log(error)
     
